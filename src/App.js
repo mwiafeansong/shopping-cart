fae76764efe2a995600fc52c5a5a3cfa6cc3c1ba
mwiafeansong <91784976+mwiafeansong@users.noreply.github.com>
2023-03-10 11:33:59 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ const App = () => {
         </div>
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/shopping-cart" element={<Home />} />
           <Route
             path="/Shop"
             element={
